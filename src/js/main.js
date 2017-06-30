eea1bbc7845567524253cffeac2bcc0299394437
Dmitry <dmitry.bochkov@gmail.com>
2017-06-30 21:17:40 +0300

    finished without rwd

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,30 @@
 //= jquery-1.12.4.min.js
 //= bootstrap.min.js
+//= jquery.easing.min.js
+//= jquery.sticky.js
+//= jquery.scrolly.js
+
+$(document).ready(function(){
+   $(".top-nav").sticky({topSpacing:0});
+
+   // smooth scroll
+   $('.nav__item a').bind('click', function(e) {
+
+     var anchor = $(this);
+     var headerHeight= '68';
+
+     $('html, body').stop().animate({
+       scrollTop: $(anchor.attr('href')).offset().top - headerHeight + 'px'
+     }, 1200, 'easeInOutSine');
+     e.preventDefault();
+   });
+
+   // scroll spy
+   $('body').scrollspy({
+     target: '.navbar-collapse',
+     offset: 95
+   })
+
+   // parallax
+   $('.parallax-bg').scrolly({bgParallax: true});
+ });
