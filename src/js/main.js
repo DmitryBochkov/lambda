6a96850b39b2c2266be489ccc45c2d13749f6778
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2017-07-04 18:50:50 +0300

    parallax, css optimized

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,8 @@
 //= jquery-1.12.4.min.js
 //= bootstrap.min.js
-//= jquery.easing.min.js
+//= ../../node_modules/jquery.easing/jquery.easing.min.js
+//= ../../node_modules/jquery.stellar/jquery.stellar.js
 //= ../../node_modules/pikaday/pikaday.js
-//= ../../node_modules/gsap/src/minified/TweenMax.min.js
-//= ../../node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js
-//= ../../node_modules/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js
 
 //date picker
 var picker = new Pikaday({
@@ -15,11 +13,12 @@ $(document).ready(function(){
 
   // fixed-nav
   function fixNav() {
-    if ($(window).scrollTop() > 0) {
-      $('body').addClass('fixed-nav');
-    } else {
-      $('body').removeClass('fixed-nav');
+    var body = $('body');
 
+    if ($(window).scrollTop() > 0) {
+      body.addClass('fixed-nav');
+    } else {
+      body.removeClass('fixed-nav');
     }
   }
 
@@ -43,20 +42,9 @@ $(document).ready(function(){
      offset: 95
    });
 
-   // Init ScrollMagic
-   var controller = new ScrollMagic.Controller();
-
-   // parallax scene
-   var parallaxTl1 = new TimelineMax();
-   parallaxTl1
-     .from('.ingredients__bg', 2, {y: '-50%', ease:Power0.easeNone}, 0) ;
-
-   var ingredientsParallaxScene = new ScrollMagic.Scene({
-     triggerElement: '#ingredients',
-     triggerHook: 1,
-     duration: '200%'
-   })
-   .setTween(parallaxTl1)
-   .addTo(controller);
-
+   // parallax
+    $(window).stellar({
+			horizontalScrolling: false,
+      responsive: true
+		});
  });
